1a9abd0a4dd98f54718d33bf880a9b5ac42944f5
dimit <dimitar_rradev@abv.bg>
2024-09-28 11:51:46 +0300

    Git training and revisiting old problems

---
+++ src/com/basicSyntaxConditionalStatementsAndLoops/exercise/SortNumbers.java
@@ -0,0 +1,22 @@
+package com.basicSyntaxConditionalStatementsAndLoops.exercise;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortNumbers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] numbers = new int[3];
+
+        for (int n = 0; n < 3; n++) {
+            numbers[n] = Integer.parseInt(reader.readLine());
+        }
+
+        Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+    }
+}
